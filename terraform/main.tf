8d7f5ebb0b6ead8b5b3540e3065d18240bea264e
Ari Chivukula <ari@chivuku.la>
2018-06-22 21:04:50 -0700

    fully applied

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,6 +75,7 @@ resource "aws_cloudfront_distribution" "qc_distribution" {
 
   viewer_certificate {
     acm_certificate_arn = "${aws_acm_certificate.qc_certificate.arn}"
+    ssl_support_method  = "sni-only"
   }
 }
 
