662fab884aa1b18b939d780e5a8783bd6dcdab46
Ari Chivukula <ari@chivuku.la>
2018-06-22 17:23:45 -0700

    bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,15 @@
 provider "aws" {}
+
+resource "aws_s3_bucket" "qc_bucket" {
+  acl    = "public-read"
+  bucket = "queer.church"
+
+  tags {
+    Name = "queerchurch"
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
